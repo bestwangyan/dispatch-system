595a40b88b8351451fd47d615ec9becfe0b9c4e5
king <bestwangyan@qq.com>
2017-03-05 13:45:53 +0800

    change server to home server

--- config/config.py
+++ config/config.py
@@ -6,7 +6,8 @@
 # CreationDate:20160909
 
 config_info={
-    'server_ip':'m1-erp-appdev3.m1.baidu.com',
+    #'server_ip':'m1-erp-appdev3.m1.baidu.com',
+    'server_ip':'192.168.0.50',
     'server_port':8888,
     'authkey':'test'
 }
